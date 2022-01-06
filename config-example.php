c835c6d877b7e6d7e6e075b3402ef127798d9981
johninamillion <johninamio@gmail.com>
2022-01-06 19:23:21 +0100

    add config.php to .gitignore file

---
+++ config-example.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Config.php
+ * Inhalte dieser Datei sollten nicht für andere Nutzer öffentlich Zugänglich sein
+ */
+
+namespace Application;
+
+// Anwendungspfade
+define( 'APPLICATION_ROOT',     __DIR__ );
+
+// Datenbank Konfiguration
+define( 'DB_NAME',  'd56jh89a' );
+define( 'DB_USER',  'd56jh89a' );
+define( 'DB_PASS',  'khs89234!71623' );
